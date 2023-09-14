1b379abd346dc9dd272f738c113611c0d423a4de
smolgrrr <smolgrrr@protonmail.com>
2023-09-14 22:26:16 +1000

    Revert "add a bunch of random shit from nostr.ch"
    This reverts commit 7dd6769a3637ce10131b327513e999654fe978c4.

--- client/src/components/PostCard/PostCard.tsx
+++ client/src/components/PostCard/PostCard.tsx
@@ -1,16 +1,11 @@
 import CardContainer from './CardContainer';
 
-interface PostCardProps {
-  content: string;
-  time: Date;
-}
+const PostCard = ({ content }: { content: string }) => {
 
-const PostCard = ({ content, time }: PostCardProps) => {
   return (
     <>
       <CardContainer>
         <div className="flex flex-col">
-          {time.toString()}
           <div className="mr-2 flex flex-col break-words">
             {content}
           </div>
--- client/src/constants/settings.ts
+++
@@ -1,4 +0,0 @@
-let filterDifficulty: number = 0;
-let difficulty: number = 16;
-let timeout: number = 5;
-
