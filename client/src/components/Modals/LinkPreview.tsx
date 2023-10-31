449f6b4bf5326a3e51308d699acde6dec8bb894a
smolgrrr <smolgrrr@protonmail.com>
2023-10-31 11:33:47 +1100

    add proxy, idk if its a good idea

--- client/src/components/Modals/LinkPreview.tsx
+++ client/src/components/Modals/LinkPreview.tsx
@@ -2,50 +2,54 @@ import { getLinkPreview } from 'link-preview-js';
 import { useState, useEffect } from 'react';
 
 
-//Need to move this all server side 
-const LinkModal = ({ url }: { url: string}) => {
-    const [linkPreview, setLinkPreview] = useState<LinkPreview | null>(null);
+const LinkModal = ({ url }: { url: string }) => {
+  const [linkPreview, setLinkPreview] = useState<LinkPreview | null>(null);
 
-    useEffect(() => {
-      getLinkPreview(url)          
+  useEffect(() => {
+    const proxyUrl = 'https://api.allorigins.win/raw?url=';
+    getLinkPreview(url)
       .then((preview) => setLinkPreview(preview as LinkPreview))
-      .catch((error) => console.error(error));
-
+      .catch(error => {
+        console.error("Error fetching original URL, trying with proxy:", error);
+        return getLinkPreview(proxyUrl + url);
+      })
+      .then((preview) => setLinkPreview(preview as LinkPreview))
+      .catch((error) => console.error("Error fetching URL with proxy:", error));
   }, [url]);
 
-  if (!linkPreview) { 
-    return <a className="hover:underline" href={url}>{url}</a>; // or some loading state
+  if (!linkPreview) {
+    return <a className='hover:underline' href={url}>{url}</a>; // or some loading state
   }
-    
+
   return (
-    <div className="link-preview p-1 bg-neutral-800 rounded-lg border border-neutral-800">
-    <a href={linkPreview.url} target="_blank" rel="noopener noreferrer" className="">
-      <img src={linkPreview.images[0]} alt={linkPreview.title} className="rounded-lg"/>
-      <div className="font-semibold text-xs text-gray-300">
-        {linkPreview.title}
-      </div>
-    </a>
-  </div>
+    <div className="link-preview p-1 bg-neutral-800 rounded-lg border border-neutral-800">  
+      <a href={linkPreview.url} target="_blank" rel="noopener noreferrer" className="">
+        <img src={linkPreview.images[0]} alt={linkPreview.title} className="rounded-lg" />
+        <div className="font-semibold text-xs text-gray-300">
+          {linkPreview.title}
+        </div>
+      </a>
+    </div>
   );
 };
 
 interface LinkPreview {
-    url: string;
-    title: string;
-    siteName?: string;
-    description?: string;
-    mediaType: string;
-    contentType?: string;
-    images: string[];
-    videos: {
-      url?: string;
-      secureUrl?: string;
-      type?: string;
-      width?: string;
-      height?: string;
-      [key: string]: any;
-    }[];
+  url: string;
+  title: string;
+  siteName?: string;
+  description?: string;
+  mediaType: string;
+  contentType?: string;
+  images: string[];
+  videos: {
+    url?: string;
+    secureUrl?: string;
+    type?: string;
+    width?: string;
+    height?: string;
     [key: string]: any;
-  }
+  }[];
+  [key: string]: any;
+}
 
 export default LinkModal;
