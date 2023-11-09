38059d94127ba07d9722ea0c66ddbe7235c8441a
smolgrrr <smolgrrr@protonmail.com>
2023-11-09 23:38:29 +1100

    this isnt gonna be fun

---
+++ client/src/components/Settings/PoWProviders.tsx
@@ -0,0 +1,12 @@
+
+
+const PoWProviders = () => {
+
+  return (
+    <div className="settings-page bg-black text-white p-8 flex flex-col h-full">
+    </div>
+
+  );
+};
+
+export default PoWProviders;
