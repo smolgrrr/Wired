6d589cf6be970e57cf059ce470056eeb01e4ba5f
jilv220 <hkfhfkcoc@protonmail.com>
2023-11-12 15:28:37 -0800

    fix emoji-picker z-index & use better icon

--- client/src/components/Forms/Emojis/emoji-picker.tsx
+++ client/src/components/Forms/Emojis/emoji-picker.tsx
@@ -1,7 +1,7 @@
 import data, { Emoji } from "@emoji-mart/data";
 import Picker from "@emoji-mart/react";
 import { RefObject } from "react";
-import customEmojis from '../custom_emojis.json';
+import customEmojis from "../custom_emojis.json";
 
 interface EmojiPickerProps {
   topOffset: number;
@@ -14,11 +14,11 @@ interface EmojiPickerProps {
 
 const customCategoryIcons = {
   categoryIcons: {
-    "poast": {
-      src: 'https://poa.st/emoji/custom/poast_hat.png',
+    poast: {
+      src: "https://poa.st/emoji/custom/poast_hat.png",
     },
   },
-}
+};
 
 export function EmojiPicker({
   topOffset,
@@ -28,11 +28,11 @@ export function EmojiPicker({
   height = 300,
   ref,
 }: EmojiPickerProps) {
-  const customEmojiList = customEmojis.map(pack => {
+  const customEmojiList = customEmojis.map((pack) => {
     return {
       id: pack.id,
       name: pack.name,
-      emojis: pack.emojis.map(e => {
+      emojis: pack.emojis.map((e) => {
         return {
           id: e.shortcode,
           name: e.shortcode,
@@ -44,9 +44,7 @@ export function EmojiPicker({
 
   return (
     <>
-      <div
-        className="z-25"
-        ref={ref}>
+      <div className="absolute z-25" ref={ref}>
         <style>
           {`
               em-emoji-picker { max-height: ${height}px; }
