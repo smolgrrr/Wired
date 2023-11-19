8d19ece870bb9ecc14b7e12b2ec5bd1eb666e4cd
smolgrrr <smolgrrr@protonmail.com>
2023-11-19 18:00:22 +1100

    try add splashbacks

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -9,9 +9,9 @@ export default function Header() {
       <div className="flex justify-between items-center h-16">
         <a href="/">
           <div className="flex items-center gap-2">
-            <img src="/tao.png" className="h-8" alt="logo" />
+            <img src="/icon.png" className="h-12" alt="logo" />
             <span className="font-semibold text-white text-sm">
-              The Anon Operation
+              The Wired
             </span>
           </div>
         </a>
