a0ebb99cf9a9c5a8ab20bec30f29bdf7ccd25ecb
smolgrrr <smolgrrr@protonmail.com>
2023-12-15 22:05:57 +1100

    fix reposts

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -21,7 +21,12 @@ const useUniqEvents = () => {
 
   const uniqEvents = uniqBy(events, "id");
 
-  const noteEvents = uniqEvents.filter(event => event.kind === 1);
+  const noteEvents = uniqEvents.filter(event => {
+    if (event.kind === 6) {
+      console.log(event);
+    }
+    return event.kind === 1 || event.kind === 6;
+  });
   const metadataEvents = uniqEvents.filter(event => event.kind === 0);
 
   return { noteEvents, metadataEvents };
